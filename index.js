699b33642d54aef71acd201b0209141bd0c7b4d1
Antonio Tomas <antoniotd87@gmail.com>
2022-10-25 21:01:10 -0500

    Layouts Template Engines y Tailwind CSS

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // const express = require("express");
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+
+// Crear la app
 const app = express();
 
+// Habilitar Pug
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// Carpeta Publica
+app.use(express.static('public'))
+
 // Routing
-app.use("/", usuarioRoutes);
+app.use("/auth", usuarioRoutes);
 // Definir un puerto y arrancarlo
 const port = 3000;
 
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,23 +1,11 @@
 import express from "express";
+import {
+  formularioLogin,
+  formularioRegistro,
+} from "../controller/usuarioController.js";
+
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("Hola Mundo en express");
-});
-router.post("/", (req, res) => {
-  res.send("Hola Mundo en express");
-});
-router.get("/nosotros", (req, res) => {
-  res.json({ msg: "Hola Mundo en nosotros" });
-});
-
-/* router
-  .route("/")
-  .get("/", function (req, res) {
-    res.send("Hola Mundo en express");
-  })
-  .post("/", function (req, res) {
-    res.send("Hola Mundo en express");
-  }); */
+router.get("/login", formularioLogin);
+router.get("/registro", formularioRegistro);
 
 export default router;
