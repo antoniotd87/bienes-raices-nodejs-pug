e26f828ac7712349942b1dd9b33e6d486293ced7
Antonio Tomas <antoniotd87@gmail.com>
2022-10-25 22:21:04 -0500

    ORM's, Bases de datos MySQL y donenv

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 // const express = require("express");
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
 
 // Crear la app
 const app = express();
+
+// Conexion a la base de datos
+
+try {
+  await db.authenticate();
+  console.log("Conexion Correcta a la Base de datos");
+} catch (error) {
+  console.log(error);
+}
 
 // Habilitar Pug
 app.set("view engine", "pug");
 app.set("views", "./views");
 
 // Carpeta Publica
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Routing
 app.use("/auth", usuarioRoutes);
