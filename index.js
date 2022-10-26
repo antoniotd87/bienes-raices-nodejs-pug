eb45764bb9f74d27f5b19d47c74b569abc102c23
Antonio Tomas <antoniotd87@gmail.com>
2022-10-25 19:37:14 -0500

    Creacion del Proyecto

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo ");
+// const express = require("express");
+import express from "express";
+const app = express();
+
+// Routing
+app.get("/", function (req, res) {
+  res.send("Hola Mundo en express");
+});
+app.get("/nosotros", function (req, res) {
+  res.json({ msg: "Hola Mundo en nosotros" });
+});
+
+// Definir un puerto y arrancarlo
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`El servidor esta corroendo en el puerto ${port}`);
+});
